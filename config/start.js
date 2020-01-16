1de84435679c50aa2a0a99d297c1306fb0d19641
weilan <17796646068@163.com>
2020-01-16 17:49:04 +0800

    *) 增加配置文件，在根目录一键install、serve、build

---
+++ config/start.js
@@ -0,0 +1,28 @@
+/**
+ *  auth: weilan
+ *  读取文件夹并运行服务
+ */
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const sub_app_ath = path.resolve();
+const sub_apps = fs.readdirSync(sub_app_ath).filter(i => /^module\-*|main/.test(i));
+
+console.log(`即将进入所有模块并启动服务：${JSON.stringify(sub_apps)} ing...`)
+
+const exec = util.promisify(require('child_process').exec);
+function start() {
+  sub_apps.forEach(async i => {
+    console.log(`${i} 开始启动 后续不再提示 请自行查看`)
+    const { stdout, stderr } = await exec('npm run serve', { cwd: path.resolve(i) });
+    console.log(stdout)
+    console.error(stderr)
+  });
+};
+start();
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
+});
