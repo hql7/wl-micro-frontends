a97eb3c16eba5cacaafa9a71f0e4e2ffe4df7825
weilan <2821435603@qq.com>
2020-04-15 22:58:42 +0800

    *) 增加官方通信示例，当官方机制存在不能多应用同时监听的bug，暂不增加文章示例

--- subapp-ui/src/main.js
+++ subapp-ui/src/main.js
@@ -14,40 +14,45 @@ let router = null;
 let instance = null;
 const __qiankun__ = window.__POWERED_BY_QIANKUN__;
 
-export async function bootstrap({ components, utils, emitFnc, pager }) {
-  // 注册主应用下发的组件
-  Vue.use(components);
-  // 把工具函数挂载在vue $mainUtils对象
-  Vue.prototype.$mainUtils = utils;
-  // 把mainEmit函数一一挂载
-  Object.keys(emitFnc).forEach(i => {
-    Vue.prototype[i] = emitFnc[i]
-  });
-  // 在子应用注册呼机
-  pager.subscribe(v => {
-    console.log(`监听到子应用${v.from}发来消息：`, v)
-    // store.dispatch('app/setToken', v.token)
-  })
-  Vue.prototype.$pager = pager;
+export async function bootstrap({ components, utils, emitFnc, pager, actions }) {
+    // 注册主应用下发的组件
+    Vue.use(components);
+    // 把工具函数挂载在vue $mainUtils对象
+    Vue.prototype.$mainUtils = utils;
+    // 把mainEmit函数一一挂载
+    Object.keys(emitFnc).forEach(i => {
+        Vue.prototype[i] = emitFnc[i]
+    });
+    // 在子应用注册呼机
+    pager.subscribe(v => {
+        console.log(`监听到子应用${v.from}发来消息：`, v)
+            // store.dispatch('app/setToken', v.token)
+    })
+    Vue.prototype.$pager = pager;
+    // 在子应用注册官方通信
+    /* actions.onGlobalStateChange((state, prev) => console.log(`子应用subapp-ui监听到来自${state.from}发来消息：`, state, prev)); */
+    Vue.prototype.$actions = actions;
 }
 
-export async function mount({ data = {}, ROUTES, routerBase } = {}) {
-  router = new VueRouter({
-    base: __qiankun__ ? routerBase : "/",
-    mode: "history",
-    routes: __qiankun__ ? routeMatch(ROUTES, routerBase) : routes
-  });
-  instance = new Vue({
-    router,
-    store,
-    render: h => h(App, { props: data })
-  }).$mount("#app");
+export async function mount({ data = {}, ROUTES, routerBase, state } = {}) {
+    router = new VueRouter({
+        base: __qiankun__ ? routerBase : "/",
+        mode: "history",
+        routes: __qiankun__ ? routeMatch(ROUTES, routerBase) : routes
+    });
+    instance = new Vue({
+        router,
+        store,
+        render: h => h(App, {
+            props: {...data, ...state }
+        })
+    }).$mount("#app");
 }
 
 export async function unmount() {
-  instance.$destroy();
-  instance = null;
-  router = null;
+    instance.$destroy();
+    instance = null;
+    router = null;
 }
 
 // 单独开发环境
